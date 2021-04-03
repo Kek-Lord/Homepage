9d34c891b3033c139917f6a9457e515ff2578b8c
Kek-Lord <matthew.tomlinson27@gmail.com>
2021-04-03 02:10:15 +0100

    Clock time updated
    Updated script to search for local time so it will display the correct time for different time zones.

--- script.js
+++ script.js
@@ -2,9 +2,10 @@ var timeDisplay = document.getElementById("time");
 
 
 function refreshTime() {
-  var dateString = new Date().toLocaleString("en-GB", {timeZone: "Europe/London"});
+  var dateString = new Date().toLocaleString();
   var formattedString = dateString.replace(", ", " - ");
   timeDisplay.innerHTML = formattedString;
 }
 
 setInterval(refreshTime, 1000);
+  
