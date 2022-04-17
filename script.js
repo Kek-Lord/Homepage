4714e03c08ed3dcedf2baf5126944e43b9fd250c
Kek-Lord <matthew.tomlinson27@gmail.com>
2022-04-17 21:33:01 +0100

    Quotes Update
    -Added quotes to show
    -Added button to display new quotes
    -Moved greeting message to below the header
    -Fixed sidebar header animation so it will no longer appear on a new line anymore.

--- script.js
+++ script.js
@@ -44,3 +44,26 @@ function toggleSidebar() {
   }
   mini = !mini;
 }
+
+var quotes = [
+  "You have passed through life without an opponent—no one can ever know what you are capable of, not even you. - Seneca",
+  "Look back over the past, with its changing empires that rose and fell, and you can foresee the future too. - Marcus Aurelius",
+  "The one who puts the finishing touches on their life each day is never short of time. - Marcus Aurelius",
+  "Begin at once to live, and count each separate day as a separate life. - Seneca",
+  "It's time you realized that you have something in you more powerful and miraculous than the things that affect you and make you dance like a puppet. - Marcus Aurelius",
+  "Whatever can happen at any time can happen today. - Seneca",
+  "They lose the day in expectation of the night, and the night in fear of the dawn. - Seneca",
+  "Today I escaped anxiety. Or no, I discarded it, because it was within me, in my own perceptions — not outside. - Marcus Aurelius",
+  "A fit body, a calm mind, a house full of love. These things cannot be bought — they must be earned. - Naval Ravikant",
+  "It never ceases to amaze me: we all love ourselves more than other people, but care more about their opinion than our own. - Marcus Aurelius",
+  "Think of the life you have lived until now as over and, as a dead man, see what's left as a bonus and live it according to Nature. - Marcus Aurelius",
+  "The happiness of your life depends upon the quality of your thoughts. - Marcus Aurelius",
+  "You could leave life right now. Let that determine what you do and say and think. - Marcus Aurelius"
+]
+
+numberOfQuotes = quotes.length;
+
+function newQuote() {
+  var randomNumber = Math.floor(Math.random() * numberOfQuotes);
+  document.getElementById("quoteDisplay").innerHTML = quotes[randomNumber];
+}
